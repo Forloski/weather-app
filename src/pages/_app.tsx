fb12180ef4359914d0f27bf388b38fe0299bc133
Forloski <f.r.orloski@gmail.com>
2021-12-28 23:33:54 -0300

    added title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,21 +5,28 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { queryClient } from "@/services/querys/queryClient";
 import Layout from "@/components/layouts/DefaultLayout";
 import theme from "@/styles/theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CssBaseline>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <ThemeProvider theme={theme}>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </ThemeProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </Hydrate>
-      </QueryClientProvider>
-    </CssBaseline>
+    <>
+      <Head>
+        <title>Weather</title>
+      </Head>
+
+      <CssBaseline>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <ThemeProvider theme={theme}>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </Hydrate>
+        </QueryClientProvider>
+      </CssBaseline>
+    </>
   );
 }
 
